08b06e4ac45b1d471e52e9394ab596adf140c57c
Ko Inoue <k.inoue@remcorp.jp>
2022-01-15 17:52:49 +0900

    done

---
+++ app/Models/Answer.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'line_user_id',
+        'step',
+        'answer',
+    ];
+
+    /**
+     * このモデルが使用するデータベース接続
+     *
+     * @var string
+     */
+    protected $connection = 'sqlite';
+
+    /**
+     * モデルに関連付けるテーブル
+     *
+     * @var string
+     */
+    protected $table = 'answers';
+
+    /**
+     * Reset step
+     *
+     * @param string
+     * @return void
+     **/
+    public function resetStep(string $line_user_id) : void
+    {
+        $this->where('line_user_id', $line_user_id)->delete();
+    }
+
+    /**
+     * Store record for next step
+     *
+     * @param int
+     * @return void
+     **/
+    public function storeNextStep(string $line_user_id, int $step) : void
+    {
+        $this->line_user_id = $line_user_id;
+        $this->step = $step;
+        $this->answer = '';
+        $this->save();
+    }
+
+    /**
+     * Store answer replied by LINE user.
+     *
+     * @param Answer
+     * @param string
+     * @return void
+     **/
+    public function storeAnswer(Answer $answer, string $postback_answer) : void
+    {
+        $answer->answer = $postback_answer;
+        $answer->save();
+    }
+}
--- app/Services/LineBotService.php
+++ app/Services/LineBotService.php
@@ -3,17 +3,22 @@
 namespace App\Services;
 
 use Illuminate\Http\Request;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use Illuminate\Support\Facades\Session;
+use App\Models\Answer;
 use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\SignatureValidator;
-use LINE\LINEBot\Event\MessageEvent\TextMessage;
 use LINE\LINEBot\Event\PostbackEvent;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
 use jcobhams\NewsApi\NewsApi;
-use Illuminate\Support\Facades\Session;
 
 class LineBotService
 {
@@ -27,10 +32,22 @@ class LineBotService
     */
     protected $bot;
 
+    /**
+    * @var Answer
+    */
+    protected $answer_model;
+
+    /**
+    * @var NewsApi
+    */
+    protected $newsapi_client;
+
     public function __construct()
     {
         $this->httpClient = new CurlHTTPClient(config('app.line_channel_access_token'));
         $this->bot = new LINEBot($this->httpClient, ['channelSecret' => config('app.line_channel_secret')]);
+        $this->answer_model = new Answer;
+        $this->newsapi_client = new NewsApi(config('app.news_api_key'));
     }
 
     /**
@@ -43,17 +60,6 @@ class LineBotService
     */
     public function eventHandler(Request $request) : int
     {
-        // Requestが来たかどうか確認する
-        $content = 'Request from LINE';
-        $param_str = json_encode($request->all());
-        $log_message =
-        <<<__EOM__
-        $content
-        $param_str
-        __EOM__;
-
-        \Log::debug($log_message);
-
         $signature = $request->header('x-line-signature');
         $this->validateSignature($request, $signature);
 
@@ -61,41 +67,50 @@ class LineBotService
 
         foreach ($events as $event) {
             $reply_token = $event->getReplyToken();
-            $reply_message = 'Please select menu. メニューから選択してください。';
-            $message_builder = new TextMessageBuilder($reply_message);
+            $message_builder = new TextMessageBuilder('Invalid operation. 無効な操作です。');
             $line_user_id = $event->getUserId();
 
             switch (true){
                 //メッセージの受信
                 case $event instanceof TextMessage:
                     if ($event->getText() === 'pick news type') {
-                        session()->forget($line_user_id);
+                        $this->answer_model->resetStep($line_user_id);
+                        $message_builder = $this->buildStep0Msg();
 
-                        $message_builder = new TemplateMessageBuilder(
-                            "Select Language / 言語選択",
-                            new ConfirmTemplateBuilder("Select Language / 言語選択", [
-                                new PostbackTemplateActionBuilder("Engish", "en"),
-                                new PostbackTemplateActionBuilder("日本語", "jp"),
-                            ])
-                        );
-
-                        Session::put($line_user_id, 'value');
+                        $this->answer_model->storeNextStep($line_user_id, 0);
                     }
                     break;
                 //選択肢とか選んだ時に受信するイベント
                 case $event instanceof PostbackEvent:
-                    $answer = $event->getPostbackData();
-                    $session = Session::get($line_user_id);
-                    \Log::debug($session);
-                    switch ($session) {
-                        case 1: // language
-                            session()->push("$line_user_id.values.1", $answer);
-                            \Log::debug(implode( ",", session()->get($line_user_id)));
+                    $postback_answer = $event->getPostbackData();
+                    $current_answer = $this->answer_model->latest()->where('answer', '')->first();
+
+                    switch ($current_answer->step) {
+                        case 0: // language
+                            $this->answer_model->storeAnswer($current_answer, $postback_answer);
+
+                            $this->answer_model->storeNextStep($line_user_id, 1);
+
+                            $message_builder = $this->buildStep1Msg();
+
                             break;
-                        case 2: // country
+                        case 1: // country
+                            $this->answer_model->storeAnswer($current_answer, $postback_answer);
+
+                            $this->answer_model->storeNextStep($line_user_id, 2);
+
+                            $message_builder = $this->buildStep2Msg();
                             break;
-                        case 3: // category(end)
-                            $newsapi = new NewsApi(config('app.news_api_key'));
+                        case 2: // category(end)
+                            $this->answer_model->storeAnswer($current_answer, $postback_answer);
+
+                            $answers = $this->answer_model->where('line_user_id', $line_user_id)->get();
+                            $category = $answers->whereStrict('step', 2)->first()->answer;
+                            $language = $answers->whereStrict('step', 0)->first()->answer;
+                            $country = $answers->whereStrict('step', 1)->first()->answer;
+                            $news = $this->newsapi_client->getSources($category, $language, $country);
+
+                            $message_builder = $this->buildResultMsg($news->sources);
                             break;
                         default:
                             # code...
@@ -122,6 +137,7 @@ class LineBotService
             if (!$response->isSucceeded()) {
                 \Log::error('Failed!' . $response->getHTTPStatus() . ' ' . $response->getRawBody());
             }
+
             return $response->getHTTPStatus();
         }
     }
@@ -146,6 +162,108 @@ class LineBotService
 
         if (!hash_equals($expect_signature, $signature)) {
             abort(400);
+        }
+    }
+
+    /**
+     * Return TemplateMessageBuilder for step0.
+     * Step0用のTemplateMessageBuilderを生成する
+     * @param void
+     * @return TemplateMessageBuilder
+    */
+    public function buildStep0Msg() : TemplateMessageBuilder
+    {
+        return new TemplateMessageBuilder(
+            "Select Language / 言語選択",
+            new ConfirmTemplateBuilder("Select Language / 言語選択", [
+                new PostbackTemplateActionBuilder("Engish", "en"),
+                new PostbackTemplateActionBuilder("French", "fr"),
+            ])
+        );
+    }
+
+    /**
+     * Return TemplateMessageBuilder for step1.
+     * Step1用のTemplateMessageBuilderを生成する
+     * @param void
+     * @return TemplateMessageBuilder
+    */
+    public function buildStep1Msg() : TemplateMessageBuilder
+    {
+        return new TemplateMessageBuilder(
+            "Which country do you watch the news for?",
+            new ButtonTemplateBuilder(
+                "Which country do you watch the news for?",
+                "Select A Country / 国選択",
+                "",
+                [
+                    new PostbackTemplateActionBuilder("United States", "us"),
+                    new PostbackTemplateActionBuilder("Japan", "jp"),
+                    new PostbackTemplateActionBuilder("Canada", "ca"),
+                ]
+            )
+        );
+    }
+
+    /**
+     * Return TemplateMessageBuilder for step2.
+     * Step2用のTemplateMessageBuilderを生成する
+     * @param void
+     * @return TemplateMessageBuilder
+    */
+    public function buildStep2Msg() : TemplateMessageBuilder
+    {
+        return new TemplateMessageBuilder(
+            "Which category?",
+            new ButtonTemplateBuilder(
+                "Which category?",
+                "Select A Category / カテゴリ選択",
+                "",
+                [
+                    new PostbackTemplateActionBuilder("Business", "business"),
+                    new PostbackTemplateActionBuilder("General", "general"),
+                    new PostbackTemplateActionBuilder("Science", "science"),
+                    new PostbackTemplateActionBuilder("Tech", "technology"),
+                ]
+            )
+        );
+    }
+
+    /**
+     * Return TemplateMessageBuilder for result.
+     * ニュース取得結果のTemplateMessageBuilderを生成する
+     * @param array
+     * @return TemplateMessageBuilder|TemplateMessageBuilder
+    */
+    public function buildResultMsg(array $sources) : mixed
+    {
+        if (empty($sources)) {
+            return new TextMessageBuilder('No result / ニュースがありませんでした');
+        } else {
+            $columns = [];
+            // カルーセルの各カラムを生成(5つまで)
+            foreach ($sources as $num => $source) {
+                if ($num > 4) {
+                    break;
+                }
+
+                $url = str_replace("\\", "", $source->url);
+                $url = str_replace("http:", "https:", $url);
+
+                $link = new UriTemplateActionBuilder('See This News', $url);
+
+                $acp = $num * 200 === 0 ? 100 : $num * 200;
+                $columns[] = new CarouselColumnTemplateBuilder(
+                    $source->name,
+                    mb_strimwidth($source->description, 0, 59, "...", 'UTF-8'),
+                    "https://placeimg.com/640/$acp/tech",
+                    [$link]
+                );
+            }
+            // カラムをカルーセルに組み込む
+            $carousel = new CarouselTemplateBuilder($columns, 'square');
+
+            return new TemplateMessageBuilder("News results", $carousel);
         }
     }
 }
